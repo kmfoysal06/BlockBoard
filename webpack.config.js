8247e94212c26d931155c699d39c43680d1ade71
kmfoysal06 <foysalkazimd01@gmail.com>
2025-10-06 00:00:20 +0600

    added webpack starter

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,96 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+/**
+ * Webpack configuration.
+ */
+const path                    = require('path')
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin')
+const CopyPlugin              = require('copy-webpack-plugin')
 
-module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'assets/js/[name].[contenthash:8].js',
-    clean: true,
-    publicPath: './'
+// JS Directory path.
+const JS_DIR    = path.resolve(__dirname, 'src/js')
+const IMG_DIR   = path.resolve(__dirname, 'src/images')
+const BUILD_DIR = path.resolve(__dirname, 'build')
+
+const entry = {
+  main: JS_DIR + '/main.js',
+}
+
+const output = {
+  path: BUILD_DIR,
+  filename: 'js/[name].js'
+}
+
+const plugins = (argv) => [
+  new MiniCssExtractPlugin({
+    filename: 'css/[name].css'
+  }),
+  new CopyPlugin({
+    patterns: [
+      {
+        from: path.join(IMG_DIR, '/'),
+        to: path.join(BUILD_DIR, 'img')
+      },
+    ]
+  })
+]
+
+const rules = [
+  {
+    test: /\.js$/,
+    include: [JS_DIR],
+    exclude: /node_modules/,
+    use: 'babel-loader'
   },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset',
-        parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
-        generator: { filename: 'assets/images/[name].[contenthash:8][ext]' }
-      },
-      // HTML files are processed by HtmlWebpackPlugin's template handling
+  {
+    test: /\.css$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      'css-loader',
+      'postcss-loader',
     ]
   },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'assets/css/[name].[contenthash:8].css' }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: path.resolve(__dirname, 'index.html'),
-      inject: 'body',
-      scriptLoading: 'defer'
-    })
-  ],
-  resolve: { extensions: ['.js'] }
-};
+  {
+    test: /\.(png|jpg|jpeg|gif|ico)$/,
+    use: {
+      loader: 'file-loader',
+      options: {
+        name: '[path][name].[ext]',
+        publicPath: 'production' === process.env.NODE_ENV ? '../' : '../../'
+      }
+    }
+  },
+]
+
+/**
+ * Since you may have to disambiguate in your webpack.config.js between development and production builds,
+ * you can export a function from your webpack configuration instead of exporting an object
+ *
+ * @param {string} env environment ( See the environment options CLI documentation for syntax examples. https://webpack.js.org/api/cli/#environment-options )
+ * @param argv options map ( This describes the options passed to webpack, with keys such as output-filename and optimize-minimize )
+ * @return {{output: *, devtool: string, entry: *, optimization: {minimizer: [*, *]}, plugins: *, module: {rules: *}, externals: {jquery: string}}}
+ *
+ * @see https://webpack.js.org/configuration/configuration-types/#exporting-a-function
+ */
+module.exports = (env, argv) => ({
+
+  entry: entry,
+
+  output: output,
+
+  /**
+   * A full SourceMap is emitted as a separate file ( e.g.  main.js.map )
+   * It adds a reference comment to the bundle so development tools know where to find it.
+   * set this to false if you don't need it
+   */
+  devtool: 'source-map',
+
+  module: {
+    rules: rules
+  },
+
+
+  plugins: plugins(argv),
+
+  externals: {
+    jquery: 'jQuery'
+  }
+})
